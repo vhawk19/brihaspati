340fc05713f2c1e88cc0232848f80068c721bec0
varun krishna s <varunhawk19@gmail.com>
2019-09-10 23:09:25 +0530

    update core.clj to run migrations before running the server

--- src/brihaspati/core.clj
+++ src/brihaspati/core.clj
@@ -1,10 +1,14 @@
 (ns brihaspati.core
   (:gen-class)
   (:require [brihaspati.http.server :as server]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [brihaspati.db.migrations :as db-migrations]
+            brihaspati.config :as config))
 
 (defn -main
   [& args]
   "starts the brihaspati webserver"
-  (server/start "9001")
-  (println "Hello, World!"))
+  (db-migrations/migrate)
+  (log/info "All migrations applied")
+  (server/start (config/port))
+  (log/info "Server started"))
