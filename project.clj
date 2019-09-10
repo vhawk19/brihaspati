f4f6113c05ec36be9d995017833edf6e08cab252
varun krishna s <varunhawk19@gmail.com>
2019-09-10 23:06:35 +0530

    update project.clj with required deps

--- project.clj
+++ project.clj
@@ -5,7 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                   [ring "1.7.1"]
-                  [org.clojure/tools.logging "0.4.1"]]
+                  [org.clojure/tools.logging "0.4.1"]
+                  [ragtime "0.8.0"]
+                  [aero "1.1.3"]]
+  
+                  :aliases {"migrate" ["run" "-m" "brihaspati.db.migrations/migrate"]
+                  "migrate-test" ["run" "-m" "brihaspati.db.migrations/migrate-test"]
+                  "rollback" ["run" "-m" "brihaspati.db.migrations/rollback"]
+                  "rollback-all-test" ["run" "-m" "brihaspati.db.migrations/rollback-all-test"]}
   :main ^:skip-aot brihaspati.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
