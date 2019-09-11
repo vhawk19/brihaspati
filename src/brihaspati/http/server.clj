b88574046abb459f723162b060db6eae6c0f7d4c
varun krishna s <varunhawk19@gmail.com>
2019-09-11 13:16:40 +0530

    update server to have routes

--- src/brihaspati/http/server.clj
+++ src/brihaspati/http/server.clj
@@ -1,6 +1,10 @@
 (ns brihaspati.http.server
-    (:require [ring.adapter.jetty :as jetty]
-            [clojure.tools.logging :as log]))
+    (:require   [ring.adapter.jetty :as jetty]
+                [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+                [ring.util.response :as response]
+                [compojure.core :refer [defroutes GET POST PUT DELETE ANY]]
+                [clojure.tools.logging :as log]
+                [brihaspati.http.handlers.questions :as questions]))
 
 
 (defn handler 
@@ -8,6 +12,14 @@
   {:status 200
     :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry</p>"
     :headers {}})
+
+(defn wrap [handler]
+        (-> handler
+            (wrap-json-body {:keywords? true})
+            (wrap-json-response)))
+
+(defroutes app-routes
+        (POST "/api/question" [] (wrap questions/create-question-handler)))
 
 (defn int-parse [port]
     (try 
@@ -17,7 +29,7 @@
 (defonce server (atom nil))
 
 (defn start [port]
-    (reset! server (jetty/run-jetty handler {:port (int-parse port)})))
+    (reset! server (jetty/run-jetty app-routes {:port (int-parse port)})))
 
 (defn stop []
     (.stop @server)
