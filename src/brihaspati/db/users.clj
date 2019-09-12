6e1941c84e2d31a16d17040b8967af216221d1c1
varun krishna s <varunhawk19@gmail.com>
2019-09-12 22:13:42 +0530

    add unit test for create users

--- src/brihaspati/db/users.clj
+++ src/brihaspati/db/users.clj
@@ -1,6 +1,13 @@
 (ns brihaspati.db.users
     (:require   [clojure.java.jdbc :as jdbc]
                 [honeysql.core :as sql]
+                [honeysql.helpers :refer [select from where sset]]
+                [honeysql.core :as sql]
                 [honeysql.helpers :refer [select from where sset]]))
 (defn create-user [user db-spec]
     (jdbc/insert! db-spec :users user))
+(defn get-user-uid [uid db-spec]
+    (first(jdbc/query db-spec (-> (select :*)
+                                  (from   :users)
+                                  (where [:= :id uid])
+                                  sql/format))))
