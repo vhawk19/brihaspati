a9eb6b8e2cc099611b7c9797cb1f0c08531861c7
varun krishna s <varunhawk19@gmail.com>
2019-09-13 19:04:34 +0530

    update migration files,fix errors

--- resources/migrations/006-users-events.up.sql
+++ resources/migrations/006-users-events.up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users-events( id  INTEGER SERIAL PRIMARY KEY, event_id INTEGER REFERENCES events(id),user_id INTEGER REFERENCES users(id));
+CREATE TABLE users_events( id  SERIAL, event_id INTEGER REFERENCES events(id),user_id INTEGER REFERENCES users(id));
