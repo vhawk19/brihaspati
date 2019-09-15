44ab6922141fae3a27ac182291ce9b3e01c7f903
varun krishna s <varunhawk19@gmail.com>
2019-09-15 23:09:58 +0530

    refactor get-question-event-id

--- src/brihaspati/http/handlers/questions.clj
+++ src/brihaspati/http/handlers/questions.clj
@@ -8,7 +8,7 @@
           request-body (:body req)]
         (response (models/create-question request-body db-spec))))
 
-(defn get-questions-event [req ]
+(defn get-questions-event-handler [req ]
   (let [db-spec (config/db-spec)
         event-id (Integer/parseInt (get-in req [:params :event-id]))]
   (response (models/get-questions-event event-id (config/db-spec)))))
