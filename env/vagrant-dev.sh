71ec10140380b6e0c4eb902180cfe88c86a9bc15
varunhawk19 <varunhawk19@gmail.com>
2020-03-27 18:39:24 +0530

    add dev env variables and setup script

---
+++ env/vagrant-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+yes | sudo apt-get install openjdk-8-jre
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod +x lein
+sudo mv lein /usr/local/bin
+lein
+sudo /vagrant/docker-compose up -d
+export port=8080
+export DB_TYPE=postgresql
+export DB_NAME=postgres
+export DB_USER=brihaspati_db
+export HOST=localhost
+export DB_PASSWORD=tara
+export DB_PORT=7001
